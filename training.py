6284853feb7e7857793b716f5a3e133b1081ee2e
angelikakwiatek <eg.angelika@gmail.com>
2020-01-26 23:48:49 +0100

    Boosting classifier

--- training.py
+++ training.py
@@ -8,6 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
 
 
 def split_a_set(popular_genre_with_plot_rnd):
@@ -51,14 +52,19 @@ def do_naive_bayes(popular_genre_with_plot_rnd, plot):
     gnb = MultinomialNB()
     gnb.fit(train_x_vectorized.toarray(), train_y)
     print(gnb.score(test_x_vectorized.toarray(), test_y))
+    gnb.predict(tv.transform([clean(plot)]).toarray())
     print(Counter(gnb.predict(train_x_vectorized.toarray())).most_common())
 
 
-def boosting():
+def boosting(popular_genre_with_plot_rnd, plot):
     tv, train_x_vectorized, test_x_vectorized, train_y, test_y = tf_idf(popular_genre_with_plot_rnd, 5000)
 
-    # gbc = XGBClassifier(n_estimators=50)
-    # gbc.fit(train_x_vectorized, train_y)
-    # gbc.score(test_x_vectorized, test_y)
+    gbc = XGBClassifier(n_estimators=1000)
+    gbc.fit(train_x_vectorized, train_y)
+    gbc.score(test_x_vectorized, test_y)
+    gbc.predict(tv.transform([clean(plot)]))
+    Counter(gbc.predict(train_x_vectorized)).most_common()
+
+
 
 
